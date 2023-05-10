bf56c7907d0d30f7ae6933e449e2742f76ddc26c
BuildTools <unconfigured@null.spigotmc.org>
2023-05-09 21:14:13 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config({ path: __dirname + "/.env" });
+const { twitterClient } = require("./twitterClient.js")
+const CronJob = require("cron").CronJob;
+
+const tweet = async () => {
+  try {
+    await twitterClient.v2.tweet("Hello world!");
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+tweet();
